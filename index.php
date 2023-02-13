d8357be51a2ab45db8925c5cbead44e1f7470a94
Mufrat Nir <121173820+Mufratnir@users.noreply.github.com>
2023-02-13 19:33:25 +0600

    php assignment

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
-$tuitionfee = 20000;
-if ($tuitionfee >= 20000){
-    echo "tuition fee 25%";
-}elseif($tuitionfee>= 10000 && $tuitionfee < 20000){
-    echo "tuition fee 20%";
-}elseif($tuitionfee >=7000 && $tuitionfee < 10000){
-    echo "tuition fee 15%";
-}elseif( $tuitionfee <7000){
-    echo "dollars invalid";
+$number =0; 
+for ($i=0; $i <100; $i++) { 
+    $number +=$i;
+}
+echo $number."\n";
+function isEven ($num){
+    if ($num % 2==0){
+        return true;
+    }else{
+        return false;
+    }
+}
+$h = 13;
+if (isEven($h)){
+    echo "{$h}is an Even Number";
+}else {
+    echo "{$h}is a odd Number";
 }

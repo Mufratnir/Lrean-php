9bc2344961ba726429c301a32d739f49300ecd58
Mufrat Nir <121173820+Mufratnir@users.noreply.github.com>
2023-02-22 10:52:21 +0600

    assignment submit

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <?php
-$number =0; 
-for ($i=0; $i <100; $i++) { 
-    $number +=$i;
+// Frist Question
+$fruit = array('orange', 'banana', 'apple', 'watermelon', 'mango', 'plum');
+usort($fruit, function ($a, $b) {
+    return strlen($a) - strlen($b);
+});
+print_r($fruit);
+
+// Second Question
+$newFruitList1 = array('orange', 'banana', 'apple');
+$newFruitList2 = array('watermelon', 'mango', 'plum');
+$newFruitFullList = array_merge($newFruitList1, $newFruitList2);
+print_r($newFruitFullList);
+
+// Third Question
+$fruit = array('orange', 'banana', 'apple', 'watermelon', 'mango', 'plum');
+print_r($fruit);
+$someFruit = array_splice($fruit, 1, 4);
+print_r($someFruit);
+
+//Froth Ouestion
+$string = "Hello World";
+echo strlen($string);
+
+//Fifth Ouestion
+function secondHighest(array $arr) {
+    sort($arr);
+    echo $arr[sizeof($arr) - 2];
 }
-echo $number."\n";
-function isEven ($num){
-    if ($num % 2==0){
-        return true;
-    }else{
-        return false;
-    }
-}
-$h = 13;
-if (isEven($h)){
-    echo "{$h}is an Even Number";
-}else {
-    echo "{$h}is a odd Number";
-}
+secondHighest(array(4, 9, 5, 2, 8, 0, 3, 22));

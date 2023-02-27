3aa6dd1a275e639996c48bfb65dfe6c429fa464e
Mufrat Nir <121173820+Mufratnir@users.noreply.github.com>
2023-02-27 19:48:06 +0600

    Module 05 assignment

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
-<?php
-// Frist Question
-$fruit = array('orange', 'banana', 'apple', 'watermelon', 'mango', 'plum');
-usort($fruit, function ($a, $b) {
-    return strlen($a) - strlen($b);
-});
-print_r($fruit);
-
-// Second Question
-$newFruitList1 = array('orange', 'banana', 'apple');
-$newFruitList2 = array('watermelon', 'mango', 'plum');
-$newFruitFullList = array_merge($newFruitList1, $newFruitList2);
-print_r($newFruitFullList);
-
-// Third Question
-$fruit = array('orange', 'banana', 'apple', 'watermelon', 'mango', 'plum');
-print_r($fruit);
-$someFruit = array_splice($fruit, 1, 4);
-print_r($someFruit);
-
-//Froth Ouestion
-$string = "Hello World";
-echo strlen($string);
-
-//Fifth Ouestion
-function secondHighest(array $arr) {
-    sort($arr);
-    echo $arr[sizeof($arr) - 2];
-}
-secondHighest(array(4, 9, 5, 2, 8, 0, 3, 22));
+<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Mon Feb 27 2023 12:35:34 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="63fca2953e7d8e17aa2fc108" data-wf-site="63fca2953e7d8efa642fc107">
+<head>
+  <meta charset="utf-8">
+  <title>Webocean&#x27;s Groovy Site</title>
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/weboceans-groovy-site.webflow.css" rel="stylesheet" type="text/css">
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+</head>
+<body>
+  <div class="from-wrapper">
+    <div class="form-block w-form">
+      <form id="email-form" name="email-form" data-name="Email Form" method="get" class="form"><label for="name">Name</label><input type="text" class="w-input" maxlength="256" name="name" data-name="Name" placeholder="" id="name"><label for="email">Email Address</label><input type="email" class="w-input" maxlength="256" name="email" data-name="Email" placeholder="" id="email" required=""><input type="submit" value="Submit" data-wait="Please wait..." class="w-button"></form>
+      <div class="w-form-done">
+        <div>Thank you! Your submission has been received!</div>
+      </div>
+      <div class="w-form-fail">
+        <div>Oops! Something went wrong while submitting the form.</div>
+      </div>
+    </div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=63fca2953e7d8efa642fc107" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
+</html>
